e69c8cb31cd5706b86ce4a52a3c23e6b9ffdbc27
Polly <polli.burjan@gmail.com>
2021-03-10 13:16:05 +0100

    Feat: Add product consistency

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -24,6 +24,8 @@ import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
+    private int categoryID = 0;
+    private int supplierID = 0;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,16 +34,16 @@ public class ProductController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(2));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(2)));
+        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("categoryList", Initializer.categoryList);
         context.setVariable("supplierList", Initializer.supplierList);
         engine.process("product/index.html", context, resp.getWriter());
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String dropdown = request.getParameter("dropdown");
+        String addToCart = request.getParameter("addToCart");
 
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
@@ -49,15 +51,35 @@ public class ProductController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
 
-        if (dropdown.equals("category")) {
-            int categoryID = Integer.parseInt(request.getParameter("categoryList"));
-            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(categoryID)));
-        } else if (dropdown.equals("supplier")) {
-            int supplierID = Integer.parseInt(request.getParameter("supplierList"));
-            context.setVariable("products", productDataStore.getBy(productSupplierDataStore.find(supplierID)));
+        if (addToCart != null) {
+            System.out.println("Cart");
         }
+
+        if (dropdown == null) {
+            if (supplierID != 0) {
+                context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(categoryID)));
+            } else if (categoryID != 0) {
+                context.setVariable("products", productDataStore.getBy(productSupplierDataStore.find(supplierID)));
+            } else {
+                context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+            }
+        } else {
+            if (dropdown.equals("category")) {
+                int categoryID = Integer.parseInt(request.getParameter("categoryList"));
+                this.categoryID = categoryID;
+                this.supplierID = 0;
+                context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(categoryID)));
+            } else if (dropdown.equals("supplier")) {
+                int supplierID = Integer.parseInt(request.getParameter("supplierList"));
+                this.supplierID = supplierID;
+                this.categoryID = 0;
+                context.setVariable("products", productDataStore.getBy(productSupplierDataStore.find(supplierID)));
+            }
+        }
+
         context.setVariable("supplierList", Initializer.supplierList);
         context.setVariable("categoryList", Initializer.categoryList);
         engine.process("product/index.html", context, response.getWriter());
     }
 }
+
