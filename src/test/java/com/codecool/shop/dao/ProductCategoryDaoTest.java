d311f29c52747a195f92e80888201ff940ea2756
CsabaToth70 <lesitocsa@gmail.com>
2021-03-22 17:59:33 +0100

    feat: add DAO test classes

---
+++ src/test/java/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductCategoryDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ src/test/java/com/codecool/shop/dao/ProductDaoTest.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void testGetBy() {
+    }
+}
