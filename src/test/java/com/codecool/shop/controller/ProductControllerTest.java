74d42851d6e3aba6576dfd865d8e09f6c757ee23
CsabaToth70 <lesitocsa@gmail.com>
2021-03-23 10:57:32 +0100

    refactor: change test targets from DAO to the controllers

---
+++ src/test/java/com/codecool/shop/controller/ProductControllerTest.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void getLastModified() {
+    }
+
+    @Test
+    void doPost() {
+    }
+
+    @Test
+    void doPut() {
+    }
+
+    @Test
+    void doDelete() {
+    }
+
+    @Test
+    void doOptions() {
+    }
+
+    @Test
+    void doTrace() {
+    }
+
+    @Test
+    void service() {
+    }
+}
