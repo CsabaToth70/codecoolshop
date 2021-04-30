e4f6913fc11a49f20fa05483ffdc01691dac1d61
CsabaToth70 <lesitocsa@gmail.com>
2021-04-30 15:24:10 +0200

    feat: add registration html page

---
+++ src/main/java/com/codecool/shop/dao/implementation/UserDaoJdbc.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.user.User;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+public class UserDaoJdbc implements UserDao {
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public Set<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public void modifyEmail(User user) {
+
+    }
+
+    @Override
+    public Optional<User> get(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(User user, String[] params) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
