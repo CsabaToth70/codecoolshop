c72085028e198579550f8c4ad36ab61c666461ff
CsabaToth70 <lesitocsa@gmail.com>
2021-05-11 10:43:30 +0200

    feat: add empty product database

--- sample_data/init_db.sql
+++ sample_data/init_db.sql
@@ -1,3 +1,34 @@
+DROP TABLE IF EXISTS items;
+
+DROP TABLE IF EXISTS suppliers;
+CREATE TABLE suppliers
+(
+    id            serial NOT NULL PRIMARY KEY,
+    name          text,
+    description   text
+);
+
+DROP TABLE IF EXISTS product_categories;
+CREATE TABLE product_categories
+(
+    id            serial NOT NULL PRIMARY KEY,
+    name          text,
+    department    text,
+    description   text
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products
+(
+    id            serial NOT NULL PRIMARY KEY,
+    name          text,
+    price         float,
+    product_category_id   integer NOT NULL,
+    supplier_id   integer NOT NULL,
+    description   text,
+    picture       text
+);
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
@@ -8,27 +39,13 @@ CREATE TABLE users
     sysadmin boolean
 );
 
-
-DROP TABLE IF EXISTS items;
-CREATE TABLE items
-(
-    id            serial NOT NULL PRIMARY KEY,
-    name          text,
-    price         float,
-    category      text,
-    supplier_name text,
-    picture       text,
-    description   text
-);
-
-
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers
 (
     id               serial  NOT NULL PRIMARY KEY,
     user_id          integer NOT NULL,
     name             text,
-    email            citext UNIQUE,
+    email            text UNIQUE,
     phone_number     text,
     billing_country  text,
     billing_city     text,
@@ -39,7 +56,7 @@ CREATE TABLE customers
     card_holder      text,
     expiring_date    text,
     code_credit_card text,
-    username_paypal  text,
+    username_paypal  text
 );
 
 DROP TABLE IF EXISTS orders;
@@ -47,7 +64,7 @@ CREATE TABLE orders
 (
     id             serial  NOT NULL PRIMARY KEY,
     customer_id    integer NOT NULL,
-    item_id        integer NOT NULL,
+    product_id     integer NOT NULL,
     quantity       integer,
     shipping_id    integer NOT NULL,
     ordering_date  timestamp without time zone,
@@ -74,7 +91,7 @@ ALTER TABLE ONLY orders
     ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(id);
 
 ALTER TABLE ONLY orders
-    ADD CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES items(id);
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products (id);
 
 ALTER TABLE ONLY orders
     ADD CONSTRAINT fk_shipping_id FOREIGN KEY (shipping_id) REFERENCES shippings(id);
@@ -82,3 +99,8 @@ ALTER TABLE ONLY orders
 ALTER TABLE ONLY shippings
     ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders(id);
 
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_product_category_id FOREIGN KEY (product_category_id) REFERENCES product_categories(id);
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) REFERENCES suppliers(id);
